84ea5095fbd461e107a75590dd0399b90aaa4f5d
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2025-02-01 05:10:21 +0100

    astro.config.mjs changed

--- ossified-osiris/astro.config.mjs
+++ ossified-osiris/astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    devToolbar: {
+        enabled: false,
+      },
+
+// Project root directory
+root: ".",
+
+outDir: "dist", // Ensure this is correctly set
+
+publicDir: "public",
+});
