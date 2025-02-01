7593283197fb78793eb418ae775a9e89b81bd403
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2025-02-01 05:45:56 +0100

    npm install @astrojs/netlify

--- ossified-osiris/astro.config.mjs
+++ ossified-osiris/astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
     devToolbar: {
         enabled: false,
       },
+
+output: "static",
+adapter: netlify(),
 
 // Project root directory
 root: ".",
