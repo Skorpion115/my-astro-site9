50d60b1888c08d8106bc3c2e89815b74c720d719
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2026-03-06 09:36:22 +0100

    Adapter eingeschaltet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-// import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.musicstudio-ziebart.de',
     output: 'static',
-    // adapter: netlify(),    // hier den Adapter eintragen
+    adapter: netlify(),    // hier den Adapter eintragen
     trailingSlash: 'always',
     devToolbar: {
     enabled: false,
