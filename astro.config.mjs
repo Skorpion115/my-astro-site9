c75a8c5058e2a06e676b05e7137d92ac07098c06
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2026-03-28 03:43:51 +0100

    Auf astro 6 update gemacht

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 // @ts-check
+//import { defineConfig, sessionDrivers } from "astro/config";
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.musicstudio-ziebart.de',
-    output: 'static',
-    adapter: netlify(),    // hier den Adapter eintragen
-    trailingSlash: 'always',
-    devToolbar: {
+  site: "https://www.musicstudio-ziebart.de",
+  output: "static",
+  adapter: netlify(), // hier den Adapter eintragen
+  trailingSlash: "always",
+  devToolbar: {
     enabled: false,
   },
+  // session: {
+  //   driver: sessionDrivers.redis({
+  //     url: process.env.REDIS_URL
+  //   }),
+  //   cookie: { secure: true },
+  //   ttl: 3600
+  // }
 });
