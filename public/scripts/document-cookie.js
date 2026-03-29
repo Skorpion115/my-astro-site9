737aebb5c5b4c9793cd846cfb6173e62678b9e95
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2026-03-29 01:22:45 +0100

    load-iframes.js delete

--- public/scripts/document-cookie.js
+++ public/scripts/document-cookie.js
@@ -1,4 +1,1 @@
-/*
-document.cookie = "cookie-name=cookie-value; SameSite=None; Secure; path=/; domain=musicstudio-ziebart.de;"; */
-
 document.cookie = "cookie-name=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=musicstudio-ziebart.de;";
