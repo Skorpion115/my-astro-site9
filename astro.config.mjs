e7581e621fa0e46aa5c0dd87aeecf10ff158f193
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2026-04-03 19:31:50 +0200

    astro.config.mjs chagend

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
+import { fileURLToPath } from 'url';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  },
+  
   site: "https://www.musicstudio-ziebart.de",
   output: "static",
   adapter: netlify(), // hier den Adapter eintragen
