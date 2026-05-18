69cb2d8f30f96b5f77968c68501685c8988e4e88
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2026-05-19 01:23:32 +0200

    Hotel California added

--- pages.js
+++ pages.js
@@ -361,5 +361,10 @@ export const pages = [
     loc: `${SITE_URL}/kurse/father-and-son-noten-tabs/`,
     changefreq: "weekly",
     lastmod: today,
+  },
+  {
+    loc: `${SITE_URL}/kurse/hotel-california-noten-tabs/`,
+    changefreq: "weekly",
+    lastmod: today,
   },    
 ];
